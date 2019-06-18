630ad4b6ef959cfc811795b681f53ffb7c2213f7
jbonez87 <jcastrillon087@gmail.com>
2019-06-18 19:34:50 -0400

    add custom star wars fonts

--- apps/star-wars/src/app/app.module.ts
+++ apps/star-wars/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CharacterComponent } from './character/character.component';
 import { CharacterDetailComponent } from './character-detail/character-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/characters', pathMatch: 'full' },
+  { path: 'characters', component: CharacterComponent }
+];
 @NgModule({
   declarations: [AppComponent, CharacterComponent, CharacterDetailComponent, MessagesComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
